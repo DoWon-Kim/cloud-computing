4db6d4c596bcf0d947f0e65676b4439650f20f51
dowon <ghkdlxmqnf@Naver.com>
2020-11-15 17:33:38 +0900

    메뉴

--- cloud.py
+++ cloud.py
@@ -1,1 +1,16 @@
-print("test")
+import boto3
+import sys
+import pprint
+
+def menu():
+    while True:
+        print("-------------------------------------------------")
+        print("1. list instance           2. available zone")
+        print("3. start instance          4. available regions")
+        print("5. stop instance           6. create instance")
+        print("7. reboot instance         8. list images")
+        print("9.                         99. quit")
+        print("-------------------------------------------------")
+
+if __name__=='__main__':
+    menu()
